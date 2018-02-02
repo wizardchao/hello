83a35fbdf38ab7bd5511e2696843b92c254c49fd
wizardchao <18317119662@139.com>
2018-02-02 10:35:25 +0800

    first commit

---
+++ src/SayHello.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chaos
+ * Date: 2018/2/2
+ * Time: 10:04
+ */
+namespace Hello;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World!';
+    }
+}
